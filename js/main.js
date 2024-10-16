ad913d45944c5ebb0cc743c95e78be872d985a83
dixiemarielaput1 <156870258+dixiemarielaput1@users.noreply.github.com>
2024-10-15 23:28:42 -0400

    Updated Layout Files
    Added syntax for all elements and sections needed for the project.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,36 @@
 (() => {
-  console.log("TenderTouch Start");
+
+    //variables
+    console.log("TenderTouch Start");
+    let button = document.querySelector("#button");
+    let burgerCon = document.querySelector(".navbar_menu");
+    const btnHandle = document.querySelectorAll('.toggle-btn');
+
+    //functions
+    function hamburgerMenu() {
+        burgerCon.classList.toggle("slide-toggle");
+    };
+
+    //event listeners
+
+    btnHandle.forEach(button => {
+      button.addEventListener('click', function() {
+        const infoBox = this.closest('.info_box_hem').querySelector('.info_box');
   
- 
-})();
+        if (infoBox.classList.contains('hidden')) {
+          infoBox.classList.remove('hidden');
+          infoBox.classList.add('visible');
+          this.textContent = '-';
+        } else {
+          infoBox.classList.remove('visible');
+          infoBox.classList.add('hidden');
+          this.textContent = '+';
+        }
+      });
+    });
+
+    button.addEventListener("click", hamburgerMenu, false);		
+
+
+
+  })();
